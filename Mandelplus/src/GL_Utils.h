f7e1363268b80bde5bf41b2210849ed8e3eef3e8
DanielEnglisch <xeroaustria@hotmail.com>
2018-01-03 15:48:11 +0100

    restructured, formatted, cleaned,...

---
+++ Mandelplus/src/GL_Utils.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+
+static  double map(double value, double inMin, double inMax, double outMin, double outMax) {
+	return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
+}
+
+
+
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	/* Error checking */
+	int res;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &res);
+	if (res == GL_FALSE) {
+		int len;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &len);
+		char* message = (char*)alloca(len * sizeof(char));
+		glGetShaderInfoLog(id, len, &len, message);
+		std::cout << "Error compiling " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader: " << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateProgram(const std::string& vert, const std::string& frag) {
+	unsigned int programId = glCreateProgram();
+	unsigned int vShader = CompileShader(GL_VERTEX_SHADER, vert);
+	unsigned int fShader = CompileShader(GL_FRAGMENT_SHADER, frag);
+	glAttachShader(programId, vShader);
+	glAttachShader(programId, fShader);
+	glLinkProgram(programId);
+	glValidateProgram(programId);
+	glDetachShader(programId, vShader);
+	glDetachShader(programId, fShader);
+
+	return programId;
+}
