44923bf59052e121c15968e02fc08115b64105a0
DanielEnglisch <xeroaustria@hotmail.com>
2018-01-04 18:51:03 +0100

    Added command line args

--- Mandelplus/src/main.cpp
+++ Mandelplus/src/main.cpp
@@ -1,7 +1,14 @@
 #include "MandelRenderer.h"
+#include <string>     // std::string, std::stod
 
-int main(void)
-{
+int main(int argc, char* argv[]){
+
+	// w,h,dx,dy,z,iter
+	if (argc != 7) {
+		std::cout << "Usage: " << argv[0] << " <width> <height> <dx> dy> <zoom> <iterations>" << std::endl;
+		return 1;
+	}
+
 	/* Spiral */
 	//double dx = 0.2549870375144766;
 	//double dy = -0.0005679790528465;
@@ -13,9 +20,9 @@ int main(void)
 	//double zoom = 50E-9;
 
 	/*  cloverleaf */
-	double dx = -0.04524074130409;
-	double dy = 0.9868162207157838;
-	double zoom = 5.0E-12;
+	//double dx = -0.04524074130409;
+	//double dy = 0.9868162207157838;
+	//double zoom = 5.0E-12;
 	
 	/* Deep Swirl */
 	//double dx = -1.315180982097868;
@@ -27,18 +34,22 @@ int main(void)
 	//double dy = 1.0375665;
 	//double zoom = 10E-7;
 
-	MandelbrotRenderer* r1{ new MandelbrotRenderer{ 100,100,1000, zoom, dx,dy } };
+	const unsigned int width = atoi(argv[1]);
+	const unsigned int height = atoi(argv[2]);
+	const double dx = std::stod(argv[3]);
+	const double dy = std::stod(argv[4]);
+	const double zoom = std::stod(argv[5]);
+	const unsigned int iterations = atoi(argv[6]);
+
+
+	MandelbrotRenderer* r1{ new MandelbrotRenderer{ width,height,iterations, zoom, dx,dy } };
 	r1->generate();
 	r1->color();
 	r1->exportPPM();
 	r1->show();
 
-	unsigned int* iterations{ r1->cloneIterationData() };
-	unsigned int size{ r1->getNumPixels() };
-	delete r1;
+ 	delete r1;
 
-	for (unsigned int i{ 0 }; i < size; ++i)
-		std::cout << iterations[i] << ", ";
-	std::cout << std::endl;
-	delete[] iterations;
+	return 0;
+
 }
